e85a2ba4eaaed2e70d55d79031e3c329f9845246
John Judge <jjudge3@wisc.edu>
2022-12-21 19:51:02 -0600

    implemented roi creation + file write, fixed layer axes choice,need to fix bugs with roi creation

--- lib/analysis/laminar_dist.py
+++ lib/analysis/laminar_dist.py
@@ -24,7 +24,7 @@ class Line:
         return dx * dx + dy * dy
 
     def is_line_in_bounds(self, w, h):
-        """ True if line is within the square w x h"""
+        """ True if line is within w x h"""
         return (0 <= self.x1 < w
                 and 0 <= self.y1 < h
                 and 0 <= self.x2 < w
@@ -46,10 +46,12 @@ class Line:
 class LaminarROI:
     """ A layer-like (as opposed to single-cell) ROI spanning the width of a cortex layer or column"""
 
-    def __init__(self, diode_numbers, img_width=80, img_height=80):
+    def __init__(self, points, img_width=80, img_height=80, input_diode_numbers=True):
         self.w = img_width
         self.h = img_height
-        self.points = self.diode_num_to_points(diode_numbers)
+        self.points = points
+        if input_diode_numbers:
+            self.points = self.diode_num_to_points(points)
         self.center = self.calculate_center_of_mass()
 
     def get_center(self):
@@ -76,8 +78,9 @@ class LaminarROI:
             pts.append([x_px, y_px])
         return pts
 
-    def is_point_at_edge(self, x, y):
-        return x == 0 or y == 0 or x == self.w - 1 or y == self.h - 1
+    def is_point_at_edge(self, x, y, tolerance=2):
+        return x <= tolerance or y <= tolerance or \
+               x >= self.w - 1 - tolerance or y >= self.h - 1 - tolerance
 
 
 class ROICreator:
@@ -86,15 +89,31 @@ class ROICreator:
         The Laminar axes input should be guaranteed
         to be in form [start, edge] vectors. This property
         is guarantteed in LayerAxes.construct_axes method """
-    def __init__(self, laminar_axes, width=80, height=80, roi_width=3):
+    def __init__(self, layer_axes, width=80, height=80, roi_width=3):
         self.w, self.h = width, height
         self.roi_width = roi_width
-        self.axis1, self.axis2 = laminar_axes
+        self.axis1, self.axis2 = layer_axes.get_layer_axes()
+        self.n_rois_created = 0
+        self.rois = []
+
+    def round_bound_w(self, v):
+        """ Round v and keep in width bounds """
+        v = round(v)
+        v = max(0, v)
+        v = min(v, self.w-1)
+        return v
+
+    def round_bound_h(self, v):
+        """ Round v and keep in height bounds """
+        v = round(v)
+        v = max(0, v)
+        v = min(v, self.h-1)
+        return v
 
     def create_roi_from_bounds(self, perpend):
         roi = []
 
-        distance = round(perpend.get_length(), ndigits=0)
+        distance = round(perpend.get_length())
 
         # start point is where perpend1 starts
         x, y = perpend.get_start_point()
@@ -105,46 +124,73 @@ class ROICreator:
             for i in range(self.roi_width):
                 x += laminar_walk_direction[0]
                 y += laminar_walk_direction[1]
-                roi.append([round(x, ndigits=0), round(y, ndigits=0)])
+                roi.append([self.round_bound_w(x), self.round_bound_h(y)])
             # increment down column now
             x += columnar_walk_direction[0]
             y += columnar_walk_direction[1]
         return roi
 
     def get_rois(self):
-        rois = self.create_rois()  # list of list of points
-
-        # convert to list of list of diode numbers
-        raise NotImplementedError
+        self.rois = self.create_rois()  # list of list of points
 
         # convert to list of LaminarROI objects
-        return rois
+        self.rois = [LaminarROI(r,
+                                img_width=self.w,
+                                input_diode_numbers=False) for r in self.rois]
+        return self.rois
 
     def create_rois(self):
         """ Returns a list of lists of points """
         rois = []
 
-        perpendicular = Line(self.axis1.get_start_point(), self.axis2.get_start_point())
+        perpendicular = Line(self.axis1.get_start_point(),
+                             self.axis2.get_start_point())
 
-        n_rois_created = 0
+        self.n_rois_created = 0
         while perpendicular.is_line_in_bounds(self.w, self.h):
 
             # increment perpendicular
-            new_start_pt = perpendicular.get_start_point() + self.axis1.get_unit_vector() * self.roi_width
-            new_end_pt = perpendicular.get_end_point() + self.axis2.get_unit_vector() * self.roi_width
+            s = perpendicular.get_start_point()
+            uv = self.axis1.get_unit_vector()
+            new_start_pt = [s[0] + uv[0] * self.roi_width,
+                            s[1] + uv[1] * self.roi_width]
+            e = perpendicular.get_end_point()
+            new_end_pt = [e[0] + uv[0] * self.roi_width,
+                          e[1] + uv[1] * self.roi_width]
             new_perpendicular = Line(new_start_pt, new_end_pt)
+
             if new_perpendicular.is_line_in_bounds(self.w, self.h):
 
                 # Using a 'walk-along-vectors' method, create roi here
                 roi = self.create_roi_from_bounds(perpendicular)
                 rois.append(roi)
-                n_rois_created += 1
+                self.n_rois_created += 1
             else:
                 break
-
             perpendicular = new_perpendicular
-
         return rois
+
+    def convert_point_to_diode_number(self, pt):
+        x, y = pt
+        dn = y * self.w + x + 1
+        return dn
+
+    def write_roi_file(self, subdir, rois_file_prefix):
+        pad_n = str(self.n_rois_created)
+        while len(pad_n) < 2:
+            pad_n = '0' + pad_n
+        roi_filename = subdir + rois_file_prefix + "_01_to_" + pad_n + ".dat"
+        with open(roi_filename, 'w') as f:
+            f.write(str(len(roi_filename)) + "\n")
+            for i in range(len(self.rois)):
+                roi = self.rois[i].get_points()
+                f.write(str(i) + "\n")
+                f.write(str(len(roi) + 1) + "\n")  # +1 for PhotoZ reason
+                f.write(str(i) + "\n")  # needed for unknown reason
+                for pt in roi:
+                    dn = self.convert_point_to_diode_number(pt)
+                    f.write(str(dn) + "\n")  # PhotoZ actually 0-indexed internally i think?
+        print("Created file:", roi_filename)
 
 
 class LaminarDistance:
@@ -191,8 +237,8 @@ class LayerAxes:
                                   img_width=self.w,
                                   img_height=self.h)
 
-    def is_point_at_edge(self, x, y):
-        return self.corners.is_point_at_edge(x, y)
+    def is_point_at_edge(self, x, y, tolerance=2):
+        return self.corners.is_point_at_edge(x, y, tolerance=tolerance)
 
     def construct_axes(self):
         """ 2 of the corners will be at the edge(s)
@@ -216,14 +262,19 @@ class LayerAxes:
 
         # Which points should go together? There are 2 possible arrangements
         # each correct pairing should minimize total segment length
-        # let's just look at axis pt 0
         dx0 = axis_pts[0][0] - edge_pts[0][0]
         dy0 = axis_pts[0][1] - edge_pts[0][1]
         dist0 = dx0 * dx0 + dy0 * dy0
+        dx0 = axis_pts[1][0] - edge_pts[1][0]
+        dy0 = axis_pts[1][1] - edge_pts[1][1]
+        dist0 += dx0 * dx0 + dy0 * dy0
 
         dx1 = axis_pts[0][0] - edge_pts[1][0]
         dy1 = axis_pts[0][1] - edge_pts[1][1]
         dist1 = dx1 * dx1 + dy1 * dy1
+        dx1 = axis_pts[1][0] - edge_pts[0][0]
+        dy1 = axis_pts[1][1] - edge_pts[0][1]
+        dist1 += dx1 * dx1 + dy1 * dy1
 
         # Important: guarantees that axes are vectors that point
         # from axis start point -> edge point.
@@ -238,8 +289,9 @@ class LayerAxes:
 class LaminarVisualization:
     """ produce a plot of SNR with the results plotted """
 
-    def __init__(self, snr, stim_point, roi_centers, corners, lines, line_colors, linewidths):
+    def __init__(self, snr, stim_point, roi_centers, corners, lines, line_colors, linewidths, save_dir="."):
         self.plot_point(stim_point)
+        self.save_dir = save_dir
         for roi in roi_centers:
             self.plot_point(roi, color='white', marker='v')
         for i in range(len(lines)):
@@ -251,6 +303,7 @@ class LaminarVisualization:
             self.plot_point(c, color='yellow')
         plt.imshow(snr)
         plt.show()
+        plt.savefig(save_dir)
 
     def plot_point(self, p, color='red', marker='*'):
         plt.plot(p[0], p[1], color=color, marker=marker)
