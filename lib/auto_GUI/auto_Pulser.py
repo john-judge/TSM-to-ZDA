1be24652f759128159a41411bab388ffaeee5375
john-judge <jjudge@uchicago.edu>
2023-11-08 13:43:35 -0600

    adding pulser images and pulser auto

--- lib/auto_GUI/auto_Pulser.py
+++ lib/auto_GUI/auto_Pulser.py
@@ -7,21 +7,32 @@ from lib.auto_GUI.auto_GUI_base import AutoGUIBase
 
 class AutoPulser(AutoGUIBase):
 
-    def __init__(self, pulser_setting_index=0):
+    def __init__(self, pulser_setting_index=0, should_create_settings=False):
         """ Pulser Setting index is the number of times to press down when selecting from Load Settings """
         self.pulser_setting_index = pulser_setting_index
         self.start_seq = "images/pulser_start_seq.png"
         self.file = "images/pulser_file.png"
-        self.load_settings_button = "images/pulser_load_settings.png"
+        self.pulser_load_settings = "images/pulser_load_settings.png"
+        self.pulser_save_settings = 'images/pulser_save_settings.png'
+        self.pulser_delete_settings = "images/pulser_delete_settings.png"
         self.pulser_select = "images/pulser_select.png"
         self.pulser_down_arrow = "images/pulser_down_arrow.png"
         self.pulser_connect_port = "images/pulser_connect_port.png"
         self.pulser_usb_port = 'images/pulser_usb_port.png'
+        self.pulser_operation_mode = 'images/pulser_operation_mode.png'
+        self.pulser_op_mode_2 = 'images/pulser_op_mode_2.png'
+        self.pulser_new_name = 'images/pulser_new_name.png'
+        self.pulser_save = 'images/pulser_save.png'
 
-    def \
-            prepare_pulser(self):
+        self.delay_settings = [0, 10, 20, 30, 40, 50, 100, 120, 140, 160]
+        self.setting_index = {}
+        self.should_create_settings = should_create_settings
+
+    def prepare_pulser(self):
         """ Run this immediately after opening Pulser (does not select Pulser)"""
         self.open_port()
+        if self.should_create_settings:
+            self.create_settings(delete_old=True)
         self.load_settings()
         self.start_sequence()
 
@@ -31,11 +42,14 @@ class AutoPulser(AutoGUIBase):
         pa.press(['enter'])
 
     def start_sequence(self):
+        pa.scroll(-10)  # scroll to expose Operation Mode
+        self.click_next_to(self.pulser_operation_mode, 50)
+        self.click_image(self.pulser_op_mode_2)
         self.click_image(self.start_seq)
 
     def load_settings(self):
         self.click_image(self.file)
-        self.click_image(self.load_settings_button)
+        self.click_image(self.pulser_load_settings)
         self.click_image(self.pulser_down_arrow)
         for _ in range(self.pulser_setting_index):
             pa.press('down')
@@ -57,5 +71,32 @@ class AutoPulser(AutoGUIBase):
                      " manually, then continue.")
         else:
             self.load_settings()
+
+    def create_settings(self, delete_old=True):
+        if delete_old:
+            self.click_image(self.file)
+            self.click_image(self.pulser_delete_settings)
+        for interval in self.delay_settings:
+            self.create_delay_setting(interval)
+
+    def create_delay_setting(self, interval):
+        self.click_next_to(self.pulser_total_trains, 50)
+        field_values = [1, 0, 0, interval, 1, 0]  # fields: total trains, TI, P1D, P1I, P2D, P2I
+        for fv in field_values:
+            pa.hotkey('ctrl', 'a')
+            time.sleep(.1)
+            pa.press(['backspace'])
+            time.sleep(.1)
+            self.type_string(str(fv))
+        self.save_setting('delay_' + str(interval) + "ms")
+
+    def save_setting(self, name):
+        self.click_image(self.file)
+        self.click_image(self.pulser_save_settings)
+        self.click_next_to(self.pulser_new_name, 50)
+        self.type_string(name)
+        self.click_image(self.pulser_save)
+
+
 
 
