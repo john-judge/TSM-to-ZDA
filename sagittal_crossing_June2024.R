cfd8a7e361979a1c0554e7ca5f4e79c1bc100789
john-judge <jjudge@uchicago.edu>
2024-06-28 16:19:09 -0500

    more sagittal crossing analysis. more correlation analysis on response variables needed; perhaps clustering on latency based on cardinality of synapticity?

--- sagittal_crossing_June2024.R
+++ sagittal_crossing_June2024.R
@@ -1,25 +1,43 @@
 library(lme4)
 library(merTools)
 library(ggplot2)
+library(report)
+library(sjPlot)
 
 data_file <- 'C:\\Users\\jjudge3\\Desktop\\Data\\mm_full_pipeline_targets\\sagittal\\sagittal_crossing_final_by_pixel.csv'
 pixel_df <- read.csv(data_file)
 pixel_df
 
+# center Distance and Distance_Along_Barrel_Axis
+pixel_df$Distance <- pixel_df$Distance - mean(pixel_df$Distance) 
+pixel_df$Distance_Along_Barrel_Axis <- pixel_df$Distance_Along_Barrel_Axis - mean(pixel_df$Distance_Along_Barrel_Axis) 
+
 # Distance_Along_Barrel_Axis is allowed to vary between groups
-mixed_model = lmer(Value ~ Distance_Along_Barrel_Axis 
-                   + Distance 
+for (i_barrel in 0:1) {
+  print(i_barrel)
+  mixed_model = lmer(Value_lat
+                     ~ Distance_Along_Barrel_Axis 
+                     + Is_Rostral_Crossing
+                     + (1 | Litter:Animal:Slice)
+                     + (1 | Litter:Animal:Slice:Loc:Barrel)
+                     , REML = T,
+                     data = pixel_df[pixel_df['Is_Home_Barrel'] == i_barrel, ])
+  print(report::report(mixed_model))
+}
+mixed_model = lmer(Value_lat
+                   ~ Distance_Along_Barrel_Axis  + Distance
                    + Is_Rostral_Crossing
-                   + Is_Home_Barrel
-                   + (1 | Litter)
-                   + (1 | Litter:Animal)
                    + (1 | Litter:Animal:Slice)
-                   + (1 | Litter:Animal:Slice:Loc)
-                   , data = pixel_df)
+                   + (1 | Litter:Animal:Slice:Loc:Barrel)
+                   , REML = T,
+                   data = pixel_df)
+print(report::report(mixed_model))
+
+
 summary(mixed_model)
 confint(mixed_model)
-ranef(mixed_model)$Date
-coef(mixed_model)$Date
+ranef(mixed_model)
+coef(mixed_model)
 
 predictInterval(mixed_model)   # for various model predictions
 
@@ -27,4 +45,8 @@ REsim(mixed_model)             # mean, median and sd of the random effect estima
 
 plotREsim(REsim(mixed_model))  # plot the interval estimates.  Intervals that do not include zero are in bold.
 
-ggplot(aes(Distance_Along_Barrel_Axis, Value), data=pixel_df)
+ggplot(aes(Distance_Along_Barrel_Axis, Value), data=pixel_df) +
+  geom_point()
+
+sjPlot::tab_model(mixed_model)
+
