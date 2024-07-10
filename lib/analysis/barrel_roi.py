eb51c3ae21ed3ded92d296db8615e448cc3a81b9
john-judge <jjudge@uchicago.edu>
2024-07-10 16:23:03 -0500

    add false discovery rate control and go to striped ROIs instead of random ROIs

---
+++ lib/analysis/barrel_roi.py
@@ -0,0 +1,86 @@
+from lib.analysis.cell_roi import RandomROISample
+from lib.analysis.laminar_dist import *
+
+
+
+class Barrel_ROI_Creator():
+
+    def __init__(self, max_rois=100):
+        self.max_rois = max_rois
+
+    def get_rand_roi_filename(self, subdir, barrel_idx, file):
+        return subdir + '/' + file.split('.dat')[0] +'ROIs-rand_' + str(barrel_idx) + '.dat'
+    
+    def get_stripe_roi_filename(self, subdir, barrel_idx, file):
+        return subdir + '/' + file.split('.dat')[0] +'ROIs-stripe_' + str(barrel_idx) + '.dat'
+    
+    def create_barreL_roi_map(self, barrel_rois):
+        barrel_roi_map = {}
+        for i in range(len(barrel_rois)):
+            for px in barrel_rois[i]:
+                px_string = str(px[0]) + ',' + str(px[1])
+                barrel_roi_map[px_string] = i
+        return barrel_roi_map
+    
+    def get_rand_rois(self, barrel_rois, roi_dimensions=1):
+        """ Creates random circular rois in the barrel roi. Barrel ROI is list of list of [x, y] points that define the barrel.
+            Returns list of rois. """
+        barrel_roi_map = self.create_barreL_roi_map(barrel_rois)
+
+        new_rois = {i: [] for i in range(len(barrel_rois))}
+        nr_map = {}
+
+        roi_sampler = RandomROISample(roi_dimensions)
+        while(any([len(new_rois[k]) < self.max_rois for k in new_rois])):
+                i, j = roi_sampler.get_random_point()
+                px_string = str(j) + ',' + str(i)
+                if px_string not in barrel_roi_map \
+                    or (j,i) in nr_map:
+                    continue
+                else:
+                    barrel_idx = barrel_roi_map[px_string]
+                    if len(new_rois[barrel_idx]) < self.max_rois:
+                        new_rois[barrel_idx].append([j, i])
+                    nr_map[(j, i)] = 1
+        return new_rois
+    
+    def get_barrel_idx_of_point(self, barrel_roi_map, px):
+        px_string = str(px[0]) + ',' + str(px[1])
+        if px_string not in barrel_roi_map:
+            return None
+        return barrel_roi_map[px_string]
+    
+    def get_striped_rois(self, barrel_rois, barrel_axis:Line, roi_dimensions=5, h=80, w=80):
+        """ Creates random striped rois perpendicular to the given barrel axis"""
+
+        barrel_roi_map = self.create_barreL_roi_map(barrel_rois)
+
+        # format of output is barrel_idx: [roi1, roi2, ...]
+        # where roi1, roi2, ... are lists of [x, y] points that define the roi
+        new_rois = {i: [] for i in range(len(barrel_rois))}
+
+        # method: loop over all points. Calculate its distance along the barrel axis and which barrel it is in
+        # then add it to the roi whose range it falls into. 
+        # ROIs are in the order of shortest to longest distance along the barrel axis
+
+        reference_point = barrel_axis.get_end_point()
+
+        for i in range(h):
+            for j in range(w):
+
+
+                dist = barrel_axis.get_displacement_along_segment(reference_point, [j, i])
+
+                # which barrel is this point in?
+                barrel_idx = self.get_barrel_idx_of_point(barrel_roi_map, [j, i])
+                if barrel_idx is None:
+                    continue
+                roi_idx = np.abs(int(dist / roi_dimensions))
+                while roi_idx >= len(new_rois[barrel_idx]):
+                    new_rois[barrel_idx].append([])
+                new_rois[barrel_idx][roi_idx].append([j, i])
+
+        return new_rois
+
+
+        
--- sagittal_crossing_June2024.R
+++ sagittal_crossing_June2024.R
@@ -4,26 +4,69 @@ library(ggplot2)
 library(report)
 library(sjPlot)
 
-data_file <- 'C:\\Users\\jjudge3\\Desktop\\Data\\mm_full_pipeline_targets\\sagittal\\sagittal_crossing_final_by_pixel.csv'
-pixel_df <- read.csv(data_file)
-pixel_df
+sag_data_file <- 'C:\\Users\\jjudge3\\Desktop\\Data\\mm_full_pipeline_targets\\sagittal\\sagittal_crossing_final_by_pixel.csv'
+sag_df <- read.csv(sag_data_file)
+sag_df
 
-# center Distance and Distance_Along_Barrel_Axis
-pixel_df$Distance <- pixel_df$Distance - mean(pixel_df$Distance) 
-pixel_df$Distance_Along_Barrel_Axis <- pixel_df$Distance_Along_Barrel_Axis - mean(pixel_df$Distance_Along_Barrel_Axis) 
+cor_data_file <- 'C:\\Users\\jjudge3\\Desktop\\Data\\mm_full_pipeline_targets\\coronal-crossing\\coronal_crossing_final_by_pixel.csv'
+cor_df <- read.csv(cor_data_file)
+cor_df
 
-# Distance_Along_Barrel_Axis is allowed to vary between groups
+all_data_file <- 'C:\\Users\\jjudge3\\Desktop\\Data\\mm_full_pipeline_targets\\coronal-crossing\\coronal_sagittal_crossing_final_by_pixel.csv'
+all_df <- read.csv(all_data_file)
+all_df
+
+print("What is the effect of sagittal/coronal?")
+mixed_model = lmer(Value_hw
+                   ~ Distance_Along_Barrel_Axis 
+                   + Is_Sagittal
+                   + (1 | Litter:Animal:Slice)
+                   + (1 | Litter:Animal:Slice:Loc)
+                   + (1 | Litter:Animal)
+                   + (1 | Litter)
+                   , REML = T,
+                   data = all_df)
+plotREsim(REsim(mixed_model)) 
+print(report::report(mixed_model))
+
+
 for (i_barrel in 0:1) {
   print(i_barrel)
+  print("Latency, DABA")
   mixed_model = lmer(Value_lat
                      ~ Distance_Along_Barrel_Axis 
+                     + Distance_To_L5
                      + Is_Rostral_Crossing
                      + (1 | Litter:Animal:Slice)
-                     + (1 | Litter:Animal:Slice:Loc:Barrel)
+                     + (1 | Litter:Animal:Slice:Loc)
                      , REML = T,
-                     data = pixel_df[pixel_df['Is_Home_Barrel'] == i_barrel, ])
+                     data = sag_df[sag_df['Is_Home_Barrel'] == i_barrel, ])
+  print(report::report(mixed_model))
+  print("Half width, DABA")
+  mixed_model = lmer(Value_hw
+                     ~ Distance_Along_Barrel_Axis 
+                     + Distance_To_L5
+                     + Is_Rostral_Crossing
+                     + (1 | Litter:Animal:Slice)
+                     + (1 | Litter:Animal:Slice:Loc)
+                     , REML = T,
+                     data = sag_df[sag_df['Is_Home_Barrel'] == i_barrel, ])
+  print(report::report(mixed_model))
+  print("Amplitude, DABA")
+  mixed_model = lmer(Value
+                     ~ Distance_Along_Barrel_Axis 
+                     + Distance_To_L5
+                     + Is_Rostral_Crossing
+                     + (1 | Litter:Animal:Slice)
+                     + (1 | Litter:Animal:Slice:Loc)
+                     , REML = T,
+                     data = sag_df[sag_df['Is_Home_Barrel'] == i_barrel, ])
   print(report::report(mixed_model))
 }
+
+
+
+
 mixed_model = lmer(Value_lat
                    ~ Distance_Along_Barrel_Axis  + Distance
                    + Is_Rostral_Crossing
