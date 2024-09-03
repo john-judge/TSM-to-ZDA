37d86b03826d17aacc8d26619cca11b88e6d24f4
john-judge <jjudge@uchicago.edu>
2024-09-03 12:12:13 -0500

    blockade and movie maker changes

--- lib/analysis/movie_maker.py
+++ lib/analysis/movie_maker.py
@@ -1,5 +1,6 @@
 import time
 import os
+import cv2
 import numpy as np
 import pandas as pd
 import pyautogui as pa
@@ -7,6 +8,8 @@ import shutil
 import imageio
 from PIL import Image, ImageDraw
 import random
+import matplotlib.pyplot as plt
+from matplotlib.figure import figaspect
 
 from lib.auto_GUI.auto_PhotoZ import AutoPhotoZ
 
@@ -21,6 +24,13 @@ class MovieMaker:
         self.frame_step_size = frame_step_size
         self.overwrite_existing = overwrite_existing
         self.ms_per_frame = ms_per_frame
+
+    def are_all_frames_present(self, output_dir, start_frame, end_frame, frame_step_size):
+        for frame in range(start_frame, end_frame+1, frame_step_size):
+            filename = output_dir + str(frame) + ".jpg"
+            if not os.path.exists(filename):
+                return False
+        return
 
     def make_movie(self):
 
@@ -112,8 +122,9 @@ class MovieMaker:
 
                     # create gif
                     created_movie = False
+                    movie_filename = output_dir + rec_id + 'movie.gif'
                     try:
-                        imageio.mimsave(output_dir + rec_id + 'movie.gif', images)
+                        imageio.mimsave(movie_filename, images)
                         print("CREATED MOVIE:", rec_id + 'movie.gif')
                         created_movie = True
 
@@ -121,6 +132,36 @@ class MovieMaker:
                         if not created_movie:
                             print("Not creating movie for " + rec_id)
                         print(e)
+                    self.add_time_annotations(movie_filename, self.start_frame, self.frame_step_size, img_filenames)
+
+    def add_time_annotations(self, movie_filename, start_frame, frame_step_size, img_filenames):
+        # add time annotations
+        frames = []
+        final_images = []
+        t_frame = 0
+
+        for filename in img_filenames:
+            img = cv2.imread(filename)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            cv2.putText(img, str(t_frame)[:3] + " ms",
+                (5, 25),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1.0,
+                (255, 255, 255)
+            )
+            t_frame += (frame_step_size / 2)
+            cv2.imwrite(filename, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+
+            final_images.append(imageio.imread(filename))
+
+        annotated_movie_filename = movie_filename.split(".")[0] + "_annotated.gif"
+        try:
+            imageio.mimsave(movie_filename, final_images)
+            print("CREATED ANNOTATED MOVIE:", annotated_movie_filename)
+        except Exception as e:
+            print("Not creating annotated movie for " + movie_filename)
+            print(e)
+
 
 
 
